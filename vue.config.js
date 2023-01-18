c36a870b0e1767488e0678879d70175fb23100c4
Victor Magalhaes <victor.barreto@buser.com.br>
2023-01-18 09:55:03 -0300

    Configurando public path para deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
+const { nodeInternals } = require('stack-utils')
+const { node } = require('webpack')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  publicPath: './' 
 })
